e28e1d56580fe800826ef267db205c959d15c46d
Tomás Manzi <tomasgsmanzi@gmail.com>
2019-10-10 09:07:14 -0300

    Questao 2 corrigida

--- Lista 2 (tgsm)/src/school/cesar/q2/EquacaoSegundoGrau.java
+++ Lista 2 (tgsm)/src/school/cesar/q2/EquacaoSegundoGrau.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class EquacaoSegundoGrau{
 
-	private double a;
-	private double b;
-	private double c;
+	public double a;
+	public double b;
+	public double c;
 	
 	EquacaoSegundoGrau(double a, double b, double c){
 		this.a = a;
@@ -15,13 +15,37 @@ public class EquacaoSegundoGrau{
 	
 	public double[] getRaizes(){
 		double[] raizes = new double[2];
-		raizes[0] = ((-b + Math.sqrt(Math.pow(b, 2) - 4*a*c))/2*a);
-		raizes[1] = ((-b - Math.sqrt(Math.pow(b, 2) - 4*a*c))/2*a);
-		return(raizes);
+		double delta;
+		delta = (Math.sqrt(Math.pow(this.b, 2) - 4*this.a*this.c));
+		if (delta > 0) {
+			raizes[0] = ((-b+delta)/2*a);
+			raizes[1] = ((-b-delta)/2*a);
+			return(raizes);
+		}
+		else {
+			System.out.println("Erro: Delta menor que 0");
+			return(null);
+		}
 	}
 	
 	public boolean equals (EquacaoSegundoGrau obj) {
-		return((this.a == obj.a) && (this.b == obj.b) && (this.c == obj.c) && (this.getRaizes() == obj.getRaizes()));
+		boolean thetruth = true;
+		if (this.a != obj.a) {
+			thetruth = false;
+		}
+		if (this.b != obj.b) {
+			thetruth = false;
+		}
+		if (this.c != obj.c) {
+			thetruth = false;
+		}
+		if (this.getRaizes()[0] != obj.getRaizes()[0]) {
+			thetruth = false;
+		}
+		if (this.getRaizes()[1] != obj.getRaizes()[1]) {
+			thetruth = false;
+		}
+		return(thetruth);
 	}
 	
 	public String toString () {
@@ -39,6 +63,7 @@ public class EquacaoSegundoGrau{
 		double c = myscan.nextDouble();
 		EquacaoSegundoGrau test = new EquacaoSegundoGrau(a, b, c);
 		System.out.print(test.toString());
+		System.out.printf("\n");
 		System.out.println("Digite o termo a na outra equacao de segundo grau: ");
 		a = myscan.nextDouble();
 		System.out.println("Digite o termo b: ");
@@ -47,6 +72,7 @@ public class EquacaoSegundoGrau{
 		c = myscan.nextDouble();
 		EquacaoSegundoGrau test2 = new EquacaoSegundoGrau(a, b, c);
 		System.out.print(test2.toString());
+		System.out.printf("\n");
 		if (test.equals(test2)) {
 			System.out.println("Sao iguais");
 		}
